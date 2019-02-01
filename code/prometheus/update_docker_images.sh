61f5479365307d8d1afb1d2475dd9b4d781e6956
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-01 16:06:12 +0000

    Set up cluster with different name

--- code/prometheus/update_docker_images.sh
+++ code/prometheus/update_docker_images.sh
@@ -1,3 +1,3 @@
-sudo docker build -t spark-prom:latest . 
+sudo docker build -t spark-prom:latest .
 sudo docker tag spark-prom:latest 172.28.128.10/library/spark-prom:latest
 sudo docker push 172.28.128.10/library/spark-prom:latest
