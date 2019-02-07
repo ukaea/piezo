a8e14f30bd0d5cdf21c6c16f4af1d71fbfdb3992
Robert Clegg <robert.clegg@tessella.com>
2019-02-07 16:44:26 +0000

    get_logs handler

---
+++ piezo_web_app/PiezoWebApp/src/handlers/get_logs.py
@@ -0,0 +1,19 @@
+from tornado_json import schema
+
+from PiezoWebApp.src.handlers.base_handler import BaseHandler
+from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_with_string_properties
+
+
+class GetLogsHandler(BaseHandler):
+    @schema.validate(
+        input_schema=create_object_schema_with_string_properties(['driver_name', 'namespace'], required=['driver_name', 'namespace']),
+        input_example={
+            'driver_name': 'example-driver',
+            'namespace': 'example-namespace'
+        }
+    )
+    def get(self, *args, **kwargs):
+        driver_name = self.get_body_attribute('driver_name', required=True)
+        namespace = self.get_body_attribute('namespace', required=True)
+        result = self._kubernetes_adapter.get_logs(driver_name, namespace)
+        return result
