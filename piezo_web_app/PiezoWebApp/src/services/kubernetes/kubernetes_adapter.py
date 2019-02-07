4fa3f7f2885e95ec58e83d0f88f26d55d40c7d03
Robert Clegg <robert.clegg@tessella.com>
2019-02-07 15:26:19 +0000

    Web app k8s adapter

---
+++ piezo_web_app/PiezoWebApp/src/services/kubernetes/kubernetes_adapter.py
@@ -0,0 +1,62 @@
+import kubernetes
+from kubernetes.client.rest import ApiException
+import requests
+
+from PiezoWebApp.src.services.kubernetes.i_kubernetes_adapter import IKubernetesAdapter
+
+
+API_BASE_URL = 'localhost:8001'
+
+# str | The custom resource's group name
+CRD_GROUP = 'sparkoperator.k8s.io'
+
+# str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
+CRD_PLURAL = 'sparkapplications'
+
+# str | The custom resource's version
+CRD_VERSION = 'v1beta1'
+
+
+class KubernetesAdapter(IKubernetesAdapter):
+    def __init__(self, logger):
+        self._logger = logger
+        config = kubernetes.config.load_kube_config()
+        api_client = kubernetes.client.ApiClient(config)
+        self._connection = kubernetes.client.CustomObjectsApi(api_client)
+
+    def delete_job(self, job_name, namespace):
+        url = f'http://${API_BASE_URL}/apis/${CRD_GROUP}/${CRD_VERSION}/namespaces/${namespace}/${CRD_PLURAL}/${job_name}'
+        try:
+            api_response = requests.delete(url)
+            return api_response.content
+        except ApiException as exception:
+            self._logger.error(f'API exception when trying to delete job "${job_name}" in namespace '
+                               f'"${namespace}": ${exception}')
+
+    def get_logs(self, driver_name, namespace):
+        try:
+            api_response = self._connection.read_namespaced_pod_log(driver_name, namespace)
+            return api_response
+        except ApiException as exception:
+            self._logger.error(f'API exception when trying to read logs for driver "${driver_name}" in namespace '
+                               f'"${namespace}": ${exception}')
+
+    def submit_job(self, body):
+        # Get the namespace from the body
+        try:
+            namespace = body['metadata']['namespace']
+        except KeyError:
+            self._logger.warning(f'Job submitted without namespace in metadata')
+        # Try to submit the job
+        try:
+            api_response = self._connection.create_namespaced_custom_object(
+                CRD_GROUP,
+                CRD_VERSION,
+                namespace,
+                CRD_PLURAL,
+                body
+            )
+            return api_response.content
+        except ApiException as exception:
+            self._logger.error(f'API exception when trying to submit job: ${exception}')
+
