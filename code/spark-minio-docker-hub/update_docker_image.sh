a9e8f5fa17ed395794782649ed3ad477c5c6ca14
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-11 16:05:47 +0000

    remove unused files

--- code/spark-minio-docker-hub/update_docker_image.sh
+++
@@ -1,3 +0,0 @@
-sudo docker build -t spark-minio-docker:latest . 
-sudo docker tag spark-minio-docker:latest 172.28.128.10/library/spark-minio-docker:latest
-sudo docker push 172.28.128.10/library/spark-minio-docker:latest
--- code/spark-minio-docker/update_docker_images.sh
+++
@@ -1,3 +0,0 @@
-sudo docker build -t spark-minio:latest . 
-sudo docker tag spark-minio:latest 172.28.128.10/library/spark-minio:latest
-sudo docker push 172.28.128.10/library/spark-minio:latest
