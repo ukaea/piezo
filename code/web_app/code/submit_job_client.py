b30b8ccca4ed39d82fb42a87b5beb84757d83566
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-11 09:59:00 +0000

    Remove redundant code

--- code/web_app/code/submit_job_client.py
+++ code/web_app/code/submit_job_client.py
@@ -18,34 +18,7 @@ group = 'sparkoperator.k8s.io' # str | The custom resource's group name
 version = 'v1beta1' # str | The custom resource's version
 namespace = 'default' # str | The custom resource's namespace
 plural = 'sparkapplications' # str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
-# body = str('apiVersion: \"sparkoperator.k8s.io/v1beta1\"\n\
-# kind: SparkApplication\n\
-#   metadata:\n\
-#   name: spark-pi\n\
-#   namespace: default\n\
-# spec:\n\
-#   type: Scala\n\
-#   mode: cluster\n\
-#   image: \"gcr.io/spark-operator/spark:v2.4.0\"\n\
-#   imagePullPolicy: Always\n\
-#   mainClass: org.apache.spark.examples.SparkPi\n\
-#   mainApplicationFile: \"local:///opt/spark/examples/jars/spark-examples_2.11-2.4.0.jar\"\n\
-#   sparkVersion: \"2.4.0\"\n\
-#   restartPolicy:\n\
-#     type: Never\n\
-#   driver:\n\
-#     cores: 0.1\n\
-#     coreLimit: \"200m\"\n\
-#     memory: \"512m\"\n\
-#     labels:\n\
-#       version: 2.4.0\n\
-#     serviceAccount: spark\n\
-#   executor:\n\
-#     cores: 1\n\
-#     instances: 2\n\
-#     memory: \"512m\"\n\
-#     labels:\n\
-#       version: 2.4.0') #  The JSON schema of the Resource to create.
+
 body = {"apiVersion": "sparkoperator.k8s.io/v1beta1",\
 "kind": "SparkApplication",\
 "metadata":\
