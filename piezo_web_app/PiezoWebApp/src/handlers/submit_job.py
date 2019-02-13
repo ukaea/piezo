aa648c17592bab2a21db311eb34e7921e928afbc
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-13 10:20:13 +0000

    Convert submit job to post request

--- piezo_web_app/PiezoWebApp/src/handlers/submit_job.py
+++ piezo_web_app/PiezoWebApp/src/handlers/submit_job.py
@@ -4,14 +4,14 @@ from PiezoWebApp.src.handlers.base_handler import BaseHandler
 from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_with_string_properties
 
 
-class GetLogsHandler(BaseHandler):
+class SubmitJobHandler(BaseHandler):
     @schema.validate(
         input_schema=create_object_schema_with_string_properties(['body'], required=['body']),
         input_example={
             'body': 'body_placeholder'  # to be replaced by list of arguments we will allow the user to configure
         }
     )
-    def get(self, *args, **kwargs):
+    def post(self, *args, **kwargs):
         body = self.get_body_attribute('body', required=True)
         result = self._kubernetes_adapter.submit_job(body)
         return result
