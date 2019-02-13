dd9da01fb25bc6c91a608fc79fba7a69edba75ca
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-13 10:04:26 +0000

    tests for delete job handler

--- piezo_web_app/PiezoWebApp/tests/handlers/delete_job_test.py
+++ piezo_web_app/PiezoWebApp/tests/handlers/delete_job_test.py
@@ -11,27 +11,28 @@ class GetLogsHandlerTest(BaseHandlerTest):
 
     @property
     def standard_request_method(self):
-        return 'GET'
+        return 'DELETE'
 
     @gen_test
-    def test_get_returns_400_when_driver_name_is_missing(self):
+    def test_delete_returns_400_when_job_name_is_missing(self):
         body = {'namespace': 'test-namespace'}
         yield self.assert_request_returns_400(body)
 
     @gen_test
-    def test_get_returns_400_when_namespace_is_missing(self):
-        body = {'driver_name': 'test-driver'}
+    def test_delete_returns_400_when_namespace_is_missing(self):
+        body = {'job_name': 'test-spark-job'}
         yield self.assert_request_returns_400(body)
 
     @gen_test
-    def test_get_returns_logs_when_successful(self):
+    def test_delete_returns_success_confirmation_when_successful(self):
         # Arrange
-        body = {'driver_name': 'test-driver', 'namespace': 'test-namespace'}
-        self.mock_kubernetes_adapter.get_logs.return_value = '{"log": "success"}'
+        body = {'job_name': 'test-spark-job', 'namespace': 'test-namespace'}
+        self.mock_kubernetes_adapter.delete_job.return_value = \
+            '{"message": "test-spark-job deleted from namespace test-namespace"}'
         # Act
         response_body, response_code = yield self.send_request(body)
         # Assert
         self.mock_kubernetes_adapter.get_logs.assert_called_once_with('test-driver', 'test-namespace')
         assert response_code == 200
         assert response_body['status'] == 'success'
-        assert response_body['data'] == '{"log": "success"}'
+        assert response_body['data'] == '{"message": "test-spark-job deleted from namespace test-namespace"}'
