8ca7d9914e486c417446c2e5bd5ad0bd78259d56
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-13 09:48:43 +0000

    Delete job handler

---
+++ piezo_web_app/PiezoWebApp/src/handlers/delete_job.py
@@ -0,0 +1,19 @@
+from tornado_json import schema
+
+from PiezoWebApp.src.handlers.base_handler import BaseHandler
+from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_with_string_properties
+
+
+class GetLogsHandler(BaseHandler):
+    @schema.validate(
+        input_schema=create_object_schema_with_string_properties(['job_name', 'namespace'], required=['job_name', 'namespace']),
+        input_example={
+            'job_name': 'example-spark-job',
+            'namespace': 'default'
+        }
+    )
+    def get(self, *args, **kwargs):
+        job_name = self.get_body_attribute('job_name', required=True)
+        namespace = self.get_body_attribute('namespace', required=True)
+        result = self._kubernetes_adapter.delete_job(job_name, namespace)
+        return result
