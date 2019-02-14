3d8511e94d280e7954b3a5cfd7a82ff29c8cd272
Robert Clegg <robert.clegg@tessella.com>
2019-02-14 13:22:44 +0000

    Tidy run_piezo.py

--- piezo_web_app/PiezoWebApp/run_piezo.py
+++ piezo_web_app/PiezoWebApp/run_piezo.py
@@ -13,35 +13,35 @@ from PiezoWebApp.src.utils.route_helper import format_route_specification
 
 def build_kubernetes_adapter():
     config = kubernetes.config.load_incluster_config()
-    kubernetes_adapter = KubernetesAdapter(config)
-    return kubernetes_adapter
+    adapter = KubernetesAdapter(config)
+    return adapter
 
 
 def build_logger(log_file_location, level):
     # Set  up the logger
-    logger = logging.getLogger("piezo-web-app")
+    log = logging.getLogger("piezo-web-app")
     formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-    logger.setLevel(level)
+    log.setLevel(level)
 
     # Set up console logging
     console = logging.StreamHandler()
     console.setFormatter(formatter)
-    logger.addHandler(console)
+    log.addHandler(console)
 
     # Set up file logging
     log_file_name = "piezo-web-app.log"
     full_path = os.path.join(log_file_location, log_file_name)
     file_handler = logging.handlers.RotatingFileHandler(full_path, maxBytes=100*1024, backupCount=10)
     file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    return logger
+    log.addHandler(file_handler)
+    return log
 
 
-def build_app(k8s_adapter, logger):
+def build_app(k8s_adapter, log):
     kubernetes_service = KubernetesService(k8s_adapter)
     container = dict(
         kubernetes_service=kubernetes_service,
-        logger=logger
+        logger=log
     )
     app = tornado.web.Application([
         (format_route_specification("deletejob"), DeleteJobHandler, container),
@@ -53,7 +53,7 @@ def build_app(k8s_adapter, logger):
 
 if __name__ == "__main__":
     kubernetes_adapter = build_kubernetes_adapter()
-    logger = build_logger()
+    logger = build_logger("/path/to/log/dir/", "INFO")
     application = build_app(kubernetes_adapter, logger)
     application.listen(8888)
     tornado.ioloop.IOLoop.current().start()
