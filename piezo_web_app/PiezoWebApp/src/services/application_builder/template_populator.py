118e481f48e16923bf4ba3ddcdd493050d368274
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-14 09:55:56 +0000

    Rename validated arguments to validated parameters dict

--- piezo_web_app/PiezoWebApp/src/services/application_builder/template_populator.py
+++ piezo_web_app/PiezoWebApp/src/services/application_builder/template_populator.py
@@ -1,70 +1,70 @@
 class TemplatePopulator:
 
-    def build_template(self, validated_arguments):
-        if validated_arguments["language"] == "Python":
-            return self.populate_python_job_template(validated_arguments)
-        return self.populate_scala_job_template(validated_arguments)
+    def build_template(self, validated_parameters_dict):
+        if validated_parameters_dict["language"].lower() == "python":
+            return self.populate_python_job_template(validated_parameters_dict)
+        return self.populate_scala_job_template(validated_parameters_dict)
 
     @staticmethod
-    def populate_python_job_template(validated_arguments):
+    def _populate_python_job_template(validated_parameters_dict):
         template = {"apiVersion": "sparkoperator.k8s.io/v1beta1",
                     "kind": "SparkApplication",
                     "metadata":
-                        {"name": validated_arguments["name"],
+                        {"name": validated_parameters_dict["name"],
                          "namespace": "default"},
                     "spec": {
                         "type": "Python",
-                        "pythonVersion": validated_arguments["pythonVersion"],
+                        "pythonVersion": validated_parameters_dict["pythonVersion"],
                         "mode": "cluster",
                         "image": "gcr.io/spark-operator/spark:v2.4.0",
                         "imagePullPolicy": "Always",
-                        "mainApplicationFile": validated_arguments["path_to_main_application_file"],
+                        "mainApplicationFile": validated_parameters_dict["path_to_main_application_file"],
                         "sparkVersion": "2.4.0",
                         "restartPolicy": {
                             "type": "Never"},
                         "driver": {
-                            "cores": validated_arguments["driver_cores"],
-                            "coreLimit": validated_arguments["driver_core_limit"],
-                            "memory": validated_arguments["driver_memory"],
+                            "cores": validated_parameters_dict["driver_cores"],
+                            "coreLimit": validated_parameters_dict["driver_core_limit"],
+                            "memory": validated_parameters_dict["driver_memory"],
                             "labels": {
                                 "version": "2.4.0"},
                             "serviceAccount": "spark"},
                         "executor": {
-                            "cores": validated_arguments["executor_cores"],
-                            "instances": validated_arguments["executors"],
-                            "memory": validated_arguments["executor_memory"],
+                            "cores": validated_parameters_dict["executor_cores"],
+                            "instances": validated_parameters_dict["executors"],
+                            "memory": validated_parameters_dict["executor_memory"],
                             "labels": {
                                 "version": "2.4.0"}}}}
         return template
 
     @staticmethod
-    def populate_scala_job_template(validated_arguments):
+    def _populate_scala_job_template(validated_paramters_dict):
         template = {"apiVersion": "sparkoperator.k8s.io/v1beta1",
                     "kind": "SparkApplication",
                     "metadata":
-                        {"name": validated_arguments["name"],
+                        {"name": validated_paramters_dict["name"],
                          "namespace": "default"},
                     "spec": {
                         "type": "Scala",
                         "mode": "cluster",
                         "image": "gcr.io/spark-operator/spark:v2.4.0",
                         "imagePullPolicy": "Always",
-                        "mainApplicationFile": validated_arguments["path_to_main_application_file"],
-                        "mainClass": validated_arguments["main_class"],
+                        "mainApplicationFile": validated_paramters_dict["path_to_main_application_file"],
+                        "mainClass": validated_paramters_dict["main_class"],
                         "sparkVersion": "2.4.0",
                         "restartPolicy": {
                             "type": "Never"},
                         "driver": {
-                            "cores": validated_arguments["driver_cores"],
-                            "coreLimit": validated_arguments["driver_core_limit"],
-                            "memory": validated_arguments["driver_memory"],
+                            "cores": validated_paramters_dict["driver_cores"],
+                            "coreLimit": validated_paramters_dict["driver_core_limit"],
+                            "memory": validated_paramters_dict["driver_memory"],
                             "labels": {
                                 "version": "2.4.0"},
                             "serviceAccount": "spark"},
                         "executor": {
-                            "cores": validated_arguments["executor_cores"],
-                            "instances": validated_arguments["executors"],
-                            "memory": validated_arguments["executor_memory"],
+                            "cores": validated_paramters_dict["executor_cores"],
+                            "instances": validated_paramters_dict["executors"],
+                            "memory": validated_paramters_dict["executor_memory"],
                             "labels": {
                                 "version": "2.4.0"}}}}
         return template
