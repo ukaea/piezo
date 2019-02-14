f9e4bf0a6020ac5d5f0dbaad048101aa6f7e61d4
Robert Clegg <robert.clegg@tessella.com>
2019-02-14 16:21:55 +0000

    Fix more flake8 issues

--- piezo_web_app/PiezoWebApp/src/services/kubernetes/i_kubernetes_adapter.py
+++ piezo_web_app/PiezoWebApp/src/services/kubernetes/i_kubernetes_adapter.py
@@ -3,6 +3,7 @@ from abc import ABCMeta, abstractmethod
 
 class IKubernetesAdapter(metaclass=ABCMeta):
 
+    # pylint: disable=too-many-arguments
     @abstractmethod
     def delete_namespaced_custom_object(self, group, version, namespace, plural, name, body):
         pass
@@ -11,6 +12,7 @@ class IKubernetesAdapter(metaclass=ABCMeta):
     def read_namespaced_pod_log(self, driver_name, namespace):
         pass
 
+    # pylint: disable=too-many-arguments
     @abstractmethod
     def create_namespaced_custom_object(self, group, version, namespace, plural, name, body):
         pass
