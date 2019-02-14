0c5dc7664bcc2d2fb8a88399e45955f6fe8282a9
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-14 11:01:00 +0000

    Add validation for language

--- piezo_web_app/PiezoWebApp/src/services/application_builder/template_populator.py
+++ piezo_web_app/PiezoWebApp/src/services/application_builder/template_populator.py
@@ -13,7 +13,11 @@ class TemplatePopulator:
 
         if validated_parameters_dict["language"].lower() == "python":
             return self._populate_python_job_template(template, validated_parameters_dict)
-        return self._populate_scala_job_template(template, validated_parameters_dict)
+        if validated_parameters_dict["language"].lower() == "scala"
+            return self._populate_scala_job_template(template, validated_parameters_dict)
+        else:
+            raise ValueError(
+                f"Invalid language {validated_parameters_dict['language']} chosen, please use one of [Python, Scala]")
 
     @staticmethod
     def _populate_python_job_template(template, validated_parameters_dict):
