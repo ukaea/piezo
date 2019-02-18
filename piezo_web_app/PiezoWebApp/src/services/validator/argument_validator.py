f759e413d26a7e0c20c9e0a2ffdc9d1fa0c0dbf6
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-18 11:11:19 +0000

    Argument validator tests

--- piezo_web_app/PiezoWebApp/src/services/validator/argument_validator.py
+++ piezo_web_app/PiezoWebApp/src/services/validator/argument_validator.py
@@ -1,10 +1,9 @@
 from PiezoWebApp.src.services.validator.spark_job_property import SparkJobProperty
-from PiezoWebApp.src.services.validator.validation_rules import ValidationRules
 
 
 class ArgumentValidator:
 
-    def __init__(self):
+    def __init__(self, validation_rules):
         self._required_from_user_args = ["name", "language", "path_to_main_app_file"]
         self._optional_from_user_args = ["driver_cores",
                                          "driver_core_limit",
@@ -12,22 +11,20 @@ class ArgumentValidator:
                                          "executors",
                                          "executor_cores",
                                          "executor_memory"]
-        self.validation_rules = ValidationRules()
+        self.validation_rules = validation_rules
 
     def validate_arguments(self, request_body):
-        # Ensure all ars needed are present
+        # Ensure all vars needed are present
         try:
             self._validate_language_requirements(request_body)
             self._check_all_required_args_are_provided(request_body)
         except ValueError:
             raise
-        # Remove unsupported args
-        args_to_validate = self._remove_unsupported_args(request_body)
+        # check for unsupported args
+        args_to_validate = self._check_for_unsupported_args(request_body)
         # Validate remaining args
-        validated_args = ArgumentValidator._check_provided_arg_values_are_valid(args_to_validate)
-        # Add default values where needed
-        full_validated_args = self._add_defaults_for_non_user_required_args(validated_args)
-        return full_validated_args
+        validated_args_dict = ArgumentValidator._check_provided_arg_values_are_valid(args_to_validate)
+        return validated_args_dict
 
     def _check_all_required_args_are_provided(self, request_body):
         for key in self._required_from_user_args:
@@ -47,10 +44,10 @@ class ArgumentValidator:
 
         if language == "python":
             request_body["language"] = "Python"
-            self._optional_from_user_args.append("python_version")
+            self._required_from_user_args.append("python_version")
         elif language == "scala":
             request_body["language"] = "Scala"
-            self._optional_from_user_args.append("main_class")
+            self._required_from_user_args.append("main_class")
 
     @staticmethod
     def _check_provided_arg_values_are_valid(request_body):
@@ -61,13 +58,9 @@ class ArgumentValidator:
                 raise
         return request_body
 
-    def _add_defaults_for_non_user_required_args(self, current_validated_args):
-        args_to_add = [arg for arg in self._optional_from_user_args if arg not in current_validated_args]
-        for arg in args_to_add:
-            arg_default = SparkJobProperty(arg).default
-            current_validated_args[arg] = arg_default
-        return current_validated_args
-
-    def _remove_unsupported_args(self, request_body):
+    def _check_for_unsupported_args(self, request_body):
         supported_args = self._required_from_user_args + self._optional_from_user_args
-        return {arg: request_body[arg] for arg in supported_args}
+        for arg in request_body:
+            if arg not in supported_args:
+                raise ValueError(f"Argument {arg} is not a supported argument")
+        return request_body
---
+++ piezo_web_app/PiezoWebApp/tests/services/validator/argument_validator.py
@@ -0,0 +1,59 @@
+import mock
+import pytest
+
+from PiezoWebApp.src.services.validator.argument_validator import ArgumentValidator
+from PiezoWebApp.src.services.validator.validation_rules import ValidationRules
+
+
+class TestArgumentValidator:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self._mock_validation_rules = mock.create_autospec(ValidationRules)
+        self._test_Argument_validator = ArgumentValidator(self._mock_validation_rules)
+
+    @pytest.mark.parametrize("request_body", [
+                             {"name": "test", "language": "scala"},
+                             {"name": "test", "path_to_main_app_file": "/path/to/app/file"},
+                             {"language": "Scala", "path_to_main_app_file": "/path/to/app/file"}]
+                             )
+    def test_check_all_required_args_are_provided_returns_value_error_if_a_required_arg_is_missing(self, request_body):
+        # Act/Assert
+        with pytest.raises(ValueError):
+            self._test_Argument_validator._check_all_required_args_are_provided(request_body)
+
+    @pytest.mark.parametrize("request_body", [{"language": "C++"}, {"language": "R"}, {"language": ""}])
+    def test_validate_language_requirements_raises_value_error_if_arg_is_not_python_or_scala(self, request_body):
+        # Act/Assert
+        with pytest.raises(ValueError):
+            self._test_Argument_validator._validate_language_requirements(request_body)
+
+    @pytest.mark.parametrize("request_body", [{"language": "PYTHON"},
+                                              {"language": "python"},
+                                              {"language": "Python"},
+                                              {"language": "PyThOn"}])
+    def test_validate_language_requirements_adds_python_version_to_optional_args_when_given_python(self, request_body):
+        # Act
+        self._test_Argument_validator._validate_language_requirements(request_body)
+        # Assert
+        assert request_body["language"] == "Python"
+        assert "python_version" in self._test_Argument_validator._required_from_user_args
+        assert "main_class" not in self._test_Argument_validator._required_from_user_args
+
+    @pytest.mark.parametrize("request_body", [{"language": "SCALA"},
+                                              {"language": "scala"},
+                                              {"language": "Scala"},
+                                              {"language": "ScAlA"}])
+    def test_validate_language_requirements_adds_main_class_to_optional_args_when_given_scala(self, request_body):
+        # Act
+        self._test_Argument_validator._validate_language_requirements(request_body)
+        # Assert
+        assert request_body["language"] == "Scala"
+        assert "main_class" in self._test_Argument_validator._required_from_user_args
+        assert "python_version" not in self._test_Argument_validator._required_from_user_args
+
+    def test_check_for_unsupported_args_throws_value_error_if_unsupported_argument_supplied(self):
+        # Arrange
+        request_body = {"unsupported_arg": "test"}
+        # Act/Assert
+        with pytest.raises(ValueError):
+            self._test_Argument_validator._check_for_unsupported_args(request_body)
