f447d498b71e343b4de2a8e9c938da993ad78702
Robert Clegg <robert.clegg@tessella.com>
2019-02-21 15:28:38 +0000

    Fix submit_job integration tests

--- piezo_web_app/PiezoWebApp/tests/integration_tests/submit_job_test.py
+++ piezo_web_app/PiezoWebApp/tests/integration_tests/submit_job_test.py
@@ -1,7 +1,16 @@
 from PiezoWebApp.src.handlers.submit_job import SubmitJobHandler
-from PiezoWebApp.tests.integration_tests.base_integration_test import *
+from PiezoWebApp.tests.integration_tests.base_integration_test import BaseIntegrationTest
 from tornado.testing import gen_test
-from types import SimpleNamespace
+
+
+# str | The custom resource's group name
+CRD_GROUP = 'sparkoperator.k8s.io'
+
+# str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
+CRD_PLURAL = 'sparkapplications'
+
+# str | The custom resource's version
+CRD_VERSION = 'v1beta1'
 
 
 class TestSubmitJobIntegration(BaseIntegrationTest):
@@ -27,35 +36,49 @@ class TestSubmitJobIntegration(BaseIntegrationTest):
         # Act
         response_body, response_code = yield self.send_request(body)
         # Assert
-        expected_body = {"apiVersion": "sparkoperator.k8s.io/v1beta1",
-                         "kind": "SparkApplication",
-                         "metadata":
-                             {"name": "test_python_job",
-                              "namespace": "default"},
-                         "spec": {
-                             "type": "Python",
-                             "pythonVersion": "2",
-                             "mode": "cluster",
-                             "image": "gcr.io/spark-operator/spark:v2.4.0",
-                             "imagePullPolicy": "Always",
-                             "mainApplicationFile": "/path_to/file",
-                             "sparkVersion": "2.4.0",
-                             "restartPolicy": {
-                                 "type": "Never"},
-                             "driver": {
-                                 "cores": 0.1,
-                                 "coreLimit": "200m",
-                                 "memory": "512m",
-                                 "labels": {
-                                     "version": "2.4.0"},
-                                 "serviceAccount": "spark"},
-                             "executor": {
-                                 "cores": 1,
-                                 "instances": 1,
-                                 "memory": "512m",
-                                 "labels": {
-                                     "version": "2.4.0"}}}}
-        # assert self.mock_k8s_adapter.create_namespaced_custom_object.assert_called_once_with(
-        #     CRD_GROUP, CRD_VERSION, "default", CRD_PLURAL, expected_body)
-        assert response_body['data']['driver_name'] == 'test_python_job-driver'
-
+        expected_body = {
+            "apiVersion": "sparkoperator.k8s.io/v1beta1",
+             "kind": "SparkApplication",
+             "metadata": {
+                 "name": "test_python_job",
+                 "namespace": "default"
+             },
+             "spec": {
+                 "type": "Python",
+                 "pythonVersion": "2",
+                 "mode": "cluster",
+                 "image": "gcr.io/spark-operator/spark:v2.4.0",
+                 "imagePullPolicy": "Always",
+                 "mainApplicationFile": "/path_to/file",
+                 "sparkVersion": "2.4.0",
+                 "restartPolicy": {"type": "Never"},
+                 "driver": {
+                     "cores": 0.1,
+                     "coreLimit": 0.2,
+                     "memory": "512m",
+                     "labels": {"version": "2.4.0"},
+                     "serviceAccount": "spark"
+                 },
+                 "executor": {
+                     "cores": 1,
+                     "instances": 1,
+                     "memory": "512m",
+                     "labels": {"version": "2.4.0"}
+                 }
+             }
+        }
+        assert self.mock_k8s_adapter.create_namespaced_custom_object.call_count == 1
+        call_args = self.mock_k8s_adapter.create_namespaced_custom_object.call_args[0]
+        assert call_args[0] == CRD_GROUP
+        assert call_args[1] == CRD_VERSION
+        assert call_args[2] == 'default'
+        assert call_args[3] == CRD_PLURAL
+        self.assertDictEqual(call_args[4], expected_body)
+        assert response_code == 200
+        self.assertDictEqual(response_body, {
+            'status': 'success',
+            'data': {
+                'message': 'Job driver created successfully',
+                'driver_name': 'test_python_job-driver'
+            }
+        })
