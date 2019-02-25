bc225be70af50b9eb481985b7c3d4dbc9ee155e2
Robert Clegg <robert.clegg@tessella.com>
2019-02-25 09:23:37 +0000

    create_object_schema_from_validation_rules helper method

--- piezo_web_app/PiezoWebApp/tests/handlers/schema/schema_helpers_test.py
+++ piezo_web_app/PiezoWebApp/tests/handlers/schema/schema_helpers_test.py
@@ -5,6 +5,109 @@ from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_
 from PiezoWebApp.src.models.spark_job_argument_classification import ArgumentClassification
 
 
+def test_create_object_schema_from_validation_rules_returns_schema_with_all_optional_properties_with_none_required():
+    # Arrange
+    rules_dict = {
+        'a': ArgumentClassification.Optional,
+        'b': ArgumentClassification.Optional,
+        'c': ArgumentClassification.Optional
+    }
+    # Act
+    schema = create_object_schema_from_validation_rules(rules_dict)
+    # Assert
+    assert schema == {
+        'type': 'object',
+        'properties': {
+            'a': {'type': 'string'},
+            'b': {'type': 'string'},
+            'c': {'type': 'string'}
+        }
+    }
+
+
+def test_create_object_schema_from_validation_rules_returns_schema_with_all_conditional_properties_with_none_required():
+    # Arrange
+    rules_dict = {
+        'a': ArgumentClassification.Conditional,
+        'b': ArgumentClassification.Conditional,
+        'c': ArgumentClassification.Conditional
+    }
+    # Act
+    schema = create_object_schema_from_validation_rules(rules_dict)
+    # Assert
+    assert schema == {
+        'type': 'object',
+        'properties': {
+            'a': {'type': 'string'},
+            'b': {'type': 'string'},
+            'c': {'type': 'string'}
+        }
+    }
+
+
+def test_create_object_schema_from_validation_rules_returns_schema_with_all_required_properties_with_all_required():
+    # Arrange
+    rules_dict = {
+        'a': ArgumentClassification.Required,
+        'b': ArgumentClassification.Required,
+        'c': ArgumentClassification.Required
+    }
+    # Act
+    schema = create_object_schema_from_validation_rules(rules_dict)
+    # Assert
+    assert schema == {
+        'type': 'object',
+        'properties': {
+            'a': {'type': 'string'},
+            'b': {'type': 'string'},
+            'c': {'type': 'string'}
+        },
+        'required': ['a', 'b', 'c']
+    }
+
+
+def test_create_object_schema_from_validation_rules_raises_value_error_with_all_fixed():
+    # Arrange
+    rules_dict = {
+        'a': ArgumentClassification.Fixed,
+        'b': ArgumentClassification.Fixed,
+        'c': ArgumentClassification.Fixed
+    }
+    # Act & Assert
+    with pytest.raises(ValueError, message='No properties provided for the schema'):
+        create_object_schema_from_validation_rules(rules_dict)
+
+
+def test_create_object_schema_from_validation_rules_raises_value_error_with_empty_dictionary():
+    # Arrange
+    rules_dict = {}
+    # Act & Assert
+    with pytest.raises(ValueError, message='No properties provided for the schema'):
+        create_object_schema_from_validation_rules(rules_dict)
+
+
+def test_create_object_schema_from_validation_rules_returns_expected_schema_with_mixed_rules():
+    # Arrange
+    rules_dict = {
+        'fixed': ArgumentClassification.Fixed,
+        'required': ArgumentClassification.Required,
+        'optional': ArgumentClassification.Optional,
+        'conditional': ArgumentClassification.Conditional
+    }
+    # Act
+    schema = create_object_schema_from_validation_rules(rules_dict)
+    # Assert
+    assert schema == {
+        'type': 'object',
+        'properties': {
+            'required': {'type': 'string'},
+            'optional': {'type': 'string'},
+            'conditional': {'type': 'string'}
+        },
+        'required': ['required']
+    }
+
+
 def test_create_object_schema_with_string_properties_returns_schema_with_all_properties_with_none_required():
     # Arrange
     properties = ['a', 'b', 'c']
