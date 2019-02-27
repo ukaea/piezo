7c92815b33f1946d6a204a1683dcbf528d61925d
Robert Clegg <robert.clegg@tessella.com>
2019-02-27 08:52:14 +0000

    Change test status error codes to dummy values

--- piezo_web_app/PiezoWebApp/tests/services/spark_job/spark_job_service_test.py
+++ piezo_web_app/PiezoWebApp/tests/services/spark_job/spark_job_service_test.py
@@ -61,7 +61,7 @@ class TestSparkJobService(TestCase):
         # Arrange
         self.mock_kubernetes_adapter.delete_namespaced_custom_object.side_effect = ApiException(
             reason="Reason",
-            status=101
+            status=999
         )
         # Act
         result = self.test_service.delete_job('test-spark-job', 'test-namespace')
@@ -69,7 +69,7 @@ class TestSparkJobService(TestCase):
         expected_message = \
             'Kubernetes error when trying to delete job "test-spark-job" in namespace "test-namespace": Reason'
         self.mock_logger.error.assert_called_once_with(expected_message)
-        assert result['status'] == 101
+        assert result['status'] == 999
         assert result['message'] == expected_message
 
     def test_get_logs_sends_expected_arguments(self):
@@ -83,7 +83,7 @@ class TestSparkJobService(TestCase):
 
     def test_get_logs_logs_and_returns_api_exception_reason(self):
         # Arrange
-        self.mock_kubernetes_adapter.read_namespaced_pod_log.side_effect = ApiException(reason="Reason", status=101)
+        self.mock_kubernetes_adapter.read_namespaced_pod_log.side_effect = ApiException(reason="Reason", status=999)
         # Act
         result = self.test_service.get_logs('test-driver', 'test-namespace')
         # Assert
@@ -91,7 +91,7 @@ class TestSparkJobService(TestCase):
             'Kubernetes error when trying to get logs for driver "test-driver" in namespace "test-namespace": Reason'
         self.mock_logger.error.assert_called_once_with(expected_message)
         self.assertDictEqual(result, {
-            'status': 101,
+            'status': 999,
             'message': 'Kubernetes error when trying to get logs for driver "test-driver" '
                        'in namespace "test-namespace": Reason'
         })
@@ -181,7 +181,7 @@ class TestSparkJobService(TestCase):
             }
         }
         self.mock_kubernetes_adapter.create_namespaced_custom_object.side_effect = \
-            ApiException(reason="Reason", status=101)
+            ApiException(reason="Reason", status=999)
         # Act
         result = self.test_service.submit_job(body)
         # Assert
@@ -197,6 +197,6 @@ class TestSparkJobService(TestCase):
             })
         ])
         self.assertDictEqual(result, {
-            'status': 101,
+            'status': 999,
             'message': expected_message
         })
