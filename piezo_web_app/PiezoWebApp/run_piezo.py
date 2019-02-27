38cd4eca17708503af9f8628090a5e64596dea46
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-02-27 11:28:15 +0000

    Use config file to build kubernetes adapter

--- piezo_web_app/PiezoWebApp/run_piezo.py
+++ piezo_web_app/PiezoWebApp/run_piezo.py
@@ -19,18 +19,22 @@ from PiezoWebApp.src.utils.route_helper import format_route_specification
 from PiezoWebApp.src.utils.configurations import Configuration
 
 
-def build_kubernetes_adapter():
-    config = kubernetes.config.load_kube_config(config_file=r"C:\Users\taro\.kube\openstack")
-    # config = kubernetes.config.load_incluster_config()
+def build_kubernetes_adapter(configuration):
+    if configuration.run_environment == "system":
+        config = kubernetes.config.load_kube_config(config_file=configuration.k8s_cluster_config_file)
+    elif configuration.run_environment == "k8s_cluster":
+        config = kubernetes.config.load_incluster_config()
+    else:
+        raise RuntimeError("Invalid running environment specified in config file")
     adapter = KubernetesAdapter(config)
     return adapter
 
 
-def build_logger(log_file_location, level):
+def build_logger(configuration):
     # Set  up the logger
     log = logging.getLogger("piezo-web-app")
     formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-    log.setLevel(level)
+    log.setLevel(configuration.logging_level)
 
     # Set up console logging
     console = logging.StreamHandler()
@@ -39,7 +43,7 @@ def build_logger(log_file_location, level):
 
     # Set up file logging
     log_file_name = "piezo-web-app.log"
-    full_path = os.path.join(log_file_location, log_file_name)
+    full_path = os.path.join(configuration.log_folder_location, log_file_name)
     file_handler = logging.handlers.RotatingFileHandler(full_path, maxBytes=100*1024, backupCount=10)
     file_handler.setFormatter(formatter)
     log.addHandler(file_handler)
@@ -73,11 +77,10 @@ def build_app(container, use_route_stem=False):
 
 
 if __name__ == "__main__":
-    # Read configuration
     configuration_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'configuration.ini'))
     CONFIGURATION = Configuration(configuration_path)
-    KUBERNETES_ADAPTER = build_kubernetes_adapter()
-    LOGGER = build_logger("/path/to/log/dir/", "INFO")
+    KUBERNETES_ADAPTER = build_kubernetes_adapter(CONFIGURATION)
+    LOGGER = build_logger(CONFIGURATION)
     CONTAINER = build_container(KUBERNETES_ADAPTER, LOGGER)
     APPLICATION = build_app(CONTAINER, use_route_stem=True)
     APPLICATION.listen(8888)
