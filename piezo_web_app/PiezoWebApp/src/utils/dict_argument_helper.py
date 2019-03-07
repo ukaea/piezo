a6a62830d8d5c6141b396988eaf952e0649864a9
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-07 09:01:01 +0000

    Alter dict helper to accept label argument

--- piezo_web_app/PiezoWebApp/src/utils/dict_argument_helper.py
+++ piezo_web_app/PiezoWebApp/src/utils/dict_argument_helper.py
@@ -5,6 +5,8 @@ def get_set_difference(first, second):
 def set_value_in_nested_dict(nested_dict, path, value):
     current_dict = nested_dict
     for i in range(len(path) - 1):
+        if path[i] not in current_dict:
+            current_dict[path[i]] = {}
         current_dict = current_dict[path[i]]
     current_dict[path[len(path) - 1]] = value
     return nested_dict
