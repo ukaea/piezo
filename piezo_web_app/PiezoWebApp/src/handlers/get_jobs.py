81a37e02742874849806256cd365f72b0095ad5d
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-07 15:10:39 +0000

    Validate output schema for handler

--- piezo_web_app/PiezoWebApp/src/handlers/get_jobs.py
+++ piezo_web_app/PiezoWebApp/src/handlers/get_jobs.py
@@ -1,8 +1,18 @@
+from tornado_json import schema
+
 from PiezoWebApp.src.handlers.base_handler import BaseHandler
 
 
 # pylint: disable=abstract-method
 class GetJobsHandler(BaseHandler):
+    @schema.validate(
+        output_schema={
+            "type": "object",
+            "properties": {
+                "message": {"type": "string"},
+            },
+        }
+    )
     def get(self, *args, **kwargs):
         result = self._spark_job_service.get_jobs()
         self._logger.debug(f'Getting list of spark applications present returned: "{result}"')
