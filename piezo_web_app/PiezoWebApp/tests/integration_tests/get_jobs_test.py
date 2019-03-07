fb80e9764195a9754aa5ba9f5713ba8f0c5858e6
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-07 16:18:28 +0000

    First integration test for handler

---
+++ piezo_web_app/PiezoWebApp/tests/integration_tests/get_jobs_test.py
@@ -0,0 +1,50 @@
+import json
+import pytest
+from tornado.httpclient import HTTPClientError
+from tornado.testing import gen_test
+from kubernetes.client.rest import ApiException
+
+from PiezoWebApp.src.handlers.get_jobs import GetJobsHandler
+from PiezoWebApp.tests.integration_tests.base_integration_test import BaseIntegrationTest
+
+
+# str | The custom resource's group name
+CRD_GROUP = 'sparkoperator.k8s.io'
+
+# str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
+CRD_PLURAL = 'sparkapplications'
+
+# str | The custom resource's version
+CRD_VERSION = 'v1beta1'
+
+
+class TestGetJobsIntegration(BaseIntegrationTest):
+    @property
+    def handler(self):
+        return GetJobsHandler
+
+    @property
+    def standard_request_method(self):
+        return 'GET'
+
+    @gen_test
+    def test_list_of_current_jobs_is_returned_as_array(self):
+        # Arrange
+        body = None
+        kubernetes_response = {"items": [{"metadata": {"name": "job1"}}, {"metadata": {"name": "job2"}}]}
+        self.mock_k8s_adapter.list_namespaced_custom_object.return_value = kubernetes_response
+        # Act
+        response_body, response_code = yield self.send_request(body)
+        # Assert
+        assert self.mock_k8s_adapter.list_namespaced_custom_object.call_count == 1
+        self.mock_k8s_adapter.list_namespaced_custom_object.assert_called_once_with(CRD_GROUP,
+                                                                                    CRD_VERSION,
+                                                                                    'default',
+                                                                                    CRD_PLURAL)
+        assert response_code == 200
+        self.assertDictEqual(response_body, {
+            'status': 'success',
+            'data': {
+                "message": "The following spark applications were found: ['job1', 'job2']"
+            }
+        })
