5965afd863d75502c6306bf450518c4938a85696
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-13 11:56:14 +0000

    Remove excess lines from docker file

---
+++ Spark/build_spark_image.sh
@@ -0,0 +1,5 @@
+HARBOR_URL=$1
+
+sudo docker build -t piezo-spark-image:latest .
+sudo docker tag piezo-spark-image:latest ${HARBOR_URL}/piezo-resources/piezo-spark-image:latest
+sudo docker push ${HARBOR_URL}/piezo-resources/piezo-spark-image:latest
