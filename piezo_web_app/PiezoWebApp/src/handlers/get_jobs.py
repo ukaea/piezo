101d9cb1cbd0d28715a751c75db8ab2eb626aa12
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-13 15:27:07 +0000

    Add label to get jobs handler

--- piezo_web_app/PiezoWebApp/src/handlers/get_jobs.py
+++ piezo_web_app/PiezoWebApp/src/handlers/get_jobs.py
@@ -1,11 +1,17 @@
 from tornado_json import schema
 
 from PiezoWebApp.src.handlers.base_handler import BaseHandler
+from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_with_string_properties
 
 
 # pylint: disable=abstract-method
 class GetJobsHandler(BaseHandler):
     @schema.validate(
+        input_schema=create_object_schema_with_string_properties(
+            ['label']),
+        input_example={
+            'label': 'test'
+        },
         output_schema={
             "type": "object",
             "properties": {
@@ -21,8 +27,9 @@ class GetJobsHandler(BaseHandler):
         }
     )
     def get(self, *args, **kwargs):
-        result = self._spark_job_service.get_jobs()
-        self._logger.debug(f'Getting list of spark applications present returned: "{result["status"]}".')
+        label = self.get_body_attribute('label', default="ALL", required=False)
+        result = self._spark_job_service.get_jobs(label)
+        self._logger.debug(f'Getting list of spark applications with label "{label}" returned: "{result["status"]}".')
         self.check_request_was_completed_successfully(result)
         del result['status']
         return result
