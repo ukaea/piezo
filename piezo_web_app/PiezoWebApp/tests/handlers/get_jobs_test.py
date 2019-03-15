22c4f8416d99fc6e50c0559e0a61e1f0d93db80f
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-15 11:07:07 +0000

    Fix handler tests

--- piezo_web_app/PiezoWebApp/tests/handlers/get_jobs_test.py
+++ piezo_web_app/PiezoWebApp/tests/handlers/get_jobs_test.py
@@ -22,13 +22,13 @@ class TestGetJobsHandler(BaseHandlerTest):
             "spark_jobs": {"job_1": "COMPLETED", "job_2": "RUNNING", "job_3": "PENDING"},
             "status": 200
         }
-        body = None
+        body = {}
         # Act
         response_body, response_code = yield self.send_request(body)
         # Assert
         self.mock_spark_job_service.get_jobs.assert_called_once()
         self.mock_logger.debug.assert_has_calls([
-            call('Getting list of spark applications present returned: "200".')
+            call('Getting list of spark applications with label "ALL" returned: "200".')
         ])
         assert response_code == 200
         self.assertDictEqual(response_body, {
@@ -47,13 +47,13 @@ class TestGetJobsHandler(BaseHandlerTest):
             "spark_jobs": {},
             "status": 200
         }
-        body = None
+        body = {}
         # Act
         response_body, response_code = yield self.send_request(body)
         # Assert
         self.mock_spark_job_service.get_jobs.assert_called_once()
         self.mock_logger.debug.assert_has_calls([
-            call('Getting list of spark applications present returned: "200".')
+            call('Getting list of spark applications with label "ALL" returned: "200".')
         ])
         assert response_code == 200
         self.assertDictEqual(response_body, {
