7ef22de054be8b1b74f4afeb424083847e4a915a
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-18 10:44:39 +0000

    Remove namespace from handlers

--- piezo_web_app/PiezoWebApp/src/handlers/delete_job.py
+++ piezo_web_app/PiezoWebApp/src/handlers/delete_job.py
@@ -8,20 +8,18 @@ from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_
 class DeleteJobHandler(BaseHandler):
     @schema.validate(
         input_schema=create_object_schema_with_string_properties(
-            ['job_name', 'namespace'],
-            required=['job_name', 'namespace']),
+            ['job_name'],
+            required=['job_name']),
         input_example={
-            'job_name': 'example-spark-job',
-            'namespace': 'default'
+            'job_name': 'example-spark-job'
         }
     )
     def delete(self, *args, **kwargs):
         job_name = self.get_body_attribute('job_name', required=True)
-        namespace = self.get_body_attribute('namespace', required=True)
-        self._logger.debug(f'Trying to delete job "{job_name}" in namespace "{namespace}".')
-        result = self._spark_job_service.delete_job(job_name, namespace)
+        self._logger.debug(f'Trying to delete job "{job_name}".')
+        result = self._spark_job_service.delete_job(job_name)
         self._logger.debug(
-            f'Deleting job "{job_name}" in namespace "{namespace}" returned result "{result["status"]}".'
+            f'Deleting job "{job_name}" returned result "{result["status"]}".'
         )
         self.check_request_was_completed_successfully(result)
         del result['status']
--- piezo_web_app/PiezoWebApp/src/handlers/job_status.py
+++ piezo_web_app/PiezoWebApp/src/handlers/job_status.py
@@ -8,19 +8,17 @@ from PiezoWebApp.src.handlers.schema.schema_helpers import create_object_schema_
 class JobStatusHandler(BaseHandler):
     @schema.validate(
         input_schema=create_object_schema_with_string_properties(
-            ['job_name', 'namespace'], required=['job_name', 'namespace']),
+            ['job_name'], required=['job_name']),
         input_example={
-            'job_name': 'example-driver',
-            'namespace': 'default'
+            'job_name': 'example-driver'
         }
     )
     def get(self, *args, **kwargs):
         spark_job = self.get_body_attribute('job_name', required=True)
-        namespace = self.get_body_attribute('namespace', required=True)
-        self._logger.debug(f'Trying to get status of spark job "{spark_job}" in namespace "{namespace}".')
-        result = self._spark_job_service.get_job_status(spark_job, namespace)
+        self._logger.debug(f'Trying to get status of spark job "{spark_job}".')
+        result = self._spark_job_service.get_job_status(spark_job)
         self._logger.debug(
-            f'Getting status of spark job "{spark_job}" in namespace "{namespace}" returned '
+            f'Getting status of spark job "{spark_job}" returned '
             f'result "{result["status"]}".'
         )
         self.check_request_was_completed_successfully(result)
