0c8fb59e8daec25a59f58a2ef147c5194174de12
Robert Clegg <robert.clegg@tessella.com>
2019-03-18 16:45:38 +0000

    Transfer modified tests to new separate class

--- piezo_web_app/PiezoWebApp/tests/services/spark_job/spark_job_service_get_jobs_test.py
+++ piezo_web_app/PiezoWebApp/tests/services/spark_job/spark_job_service_get_jobs_test.py
@@ -12,7 +12,7 @@ class SparkJobServiceGetJobsTest(TestSparkJobService):
         # Arrange
         self.mock_kubernetes_adapter.list_namespaced_custom_object.return_value = {"items": []}
         # Act
-        result = self.test_service.get_jobs()
+        result = self.test_service.get_jobs(None)
         # Assert
         self.assertDictEqual(result, {'message': 'Found 0 spark jobs', 'spark_jobs': {}, 'status': 200})
         self.mock_kubernetes_adapter.list_namespaced_custom_object.assert_called_once_with(
@@ -23,7 +23,7 @@ class SparkJobServiceGetJobsTest(TestSparkJobService):
         self.mock_kubernetes_adapter.list_namespaced_custom_object.side_effect = \
             ApiException(reason="Reason", status=999)
         # Act
-        result = self.test_service.get_jobs()
+        result = self.test_service.get_jobs(None)
         # Assert
         expected_message = \
             'Kubernetes error when trying to get a list of current spark jobs: Reason'
@@ -39,7 +39,7 @@ class SparkJobServiceGetJobsTest(TestSparkJobService):
             {"metadata": {"name": "job1"}, "status": {"applicationState": {"state": "RUNNING"}}}
         ]}
         # Act
-        result = self.test_service.get_jobs()
+        result = self.test_service.get_jobs(None)
         # Assert
         self.assertDictEqual(result, {'message': 'Found 1 spark jobs',
                                       'spark_jobs': {"job1": "RUNNING"},
@@ -47,13 +47,28 @@ class SparkJobServiceGetJobsTest(TestSparkJobService):
         self.mock_kubernetes_adapter.list_namespaced_custom_object.assert_called_once_with(
             CRD_GROUP, CRD_VERSION, 'default', CRD_PLURAL)
 
+    def test_get_jobs_only_with_user_label_when_specified(self):
+        # Arrange
+        self.mock_kubernetes_adapter.list_namespaced_custom_object.return_value = {"items": [
+            {"metadata": {"name": "job1", "labels": {"userLabel": "test-label"}},
+             "status": {"applicationState": {"state": "RUNNING"}}}
+        ]}
+        # Act
+        result = self.test_service.get_jobs("test-label")
+        # Assert
+        self.assertDictEqual(result, {'message': 'Found 1 spark jobs',
+                                      'spark_jobs': {"job1": "RUNNING"},
+                                      'status': 200})
+        self.mock_kubernetes_adapter.list_namespaced_custom_object.assert_called_once_with(
+            CRD_GROUP, CRD_VERSION, 'default', CRD_PLURAL, label_selector="userLabel=test-label")
+
     def test_get_jobs_returns_status_of_job_as_unknown_when_missing(self):
         # Arrange
         self.mock_kubernetes_adapter.list_namespaced_custom_object.return_value = {"items": [
             {"metadata": {"name": "job1"}}
         ]}
         # Act
-        result = self.test_service.get_jobs()
+        result = self.test_service.get_jobs(None)
         # Assert
         self.assertDictEqual(result, {'message': 'Found 1 spark jobs',
                                       'spark_jobs': {"job1": "UNKNOWN"},
