0a221efc3398a97969b8d0d568fa4553bfd1bbeb
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-18 10:11:03 +0000

    Don't allow users to use label all

--- piezo_web_app/PiezoWebApp/src/services/spark_job/validation/argument_validator.py
+++ piezo_web_app/PiezoWebApp/src/services/spark_job/validation/argument_validator.py
@@ -5,7 +5,9 @@ from PiezoWebApp.src.models.spark_job_validation_result import ValidationResult
 def validate(key, value, validation_rule):
     if key == "name":
         return _validate_name(value)
-    if key in ["name", "path_to_main_app_file", "main_class", "label"]:
+    if key == "label":
+        return _validate_label(value)
+    if key in ["name", "path_to_main_app_file", "main_class"]:
         return _validate_non_empty_string(key, value)
     if key in ["language", "python_version"]:
         return _validate_string_from_list(key, value, validation_rule)
@@ -18,6 +20,16 @@ def validate(key, value, validation_rule):
     if key in ["arguments"]:
         return ValidationResult(True, None, value)
     raise ValueError(f"Unexpected argument {key}")
+
+
+def _validate_label(value):
+    if not isinstance(value, str):
+        return ValidationResult(False, f'"{key}" input must be a string', None)
+    if is_str_empty(value):
+        return ValidationResult(False, f'"{key}" input cannot be empty', None)
+    if value.upper() == "ALL":
+        return ValidationResult(False, f'"{key}" input cannot be "ALL"', None)
+    return ValidationResult(True, None, value)
 
 
 def _validate_name(value):
