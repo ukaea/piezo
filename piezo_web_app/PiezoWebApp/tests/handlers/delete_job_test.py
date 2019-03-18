0e0b4325798424f92b155365a7ad7a01c1dae916
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-18 12:36:24 +0000

    Remove namespace from delete jobs test

--- piezo_web_app/PiezoWebApp/tests/handlers/delete_job_test.py
+++ piezo_web_app/PiezoWebApp/tests/handlers/delete_job_test.py
@@ -19,42 +19,37 @@ class TestDeleteJobHandler(BaseHandlerTest):
 
     @gen_test
     def test_delete_returns_400_when_job_name_is_missing(self):
-        body = {'namespace': 'test-namespace'}
-        yield self.assert_request_returns_400(body)
-
-    @gen_test
-    def test_delete_returns_400_when_namespace_is_missing(self):
-        body = {'job_name': 'test-spark-job'}
+        body = {}
         yield self.assert_request_returns_400(body)
 
     @gen_test
     def test_delete_returns_success_confirmation_when_successful(self):
         # Arrange
-        body = {'job_name': 'test-spark-job', 'namespace': 'test-namespace'}
+        body = {'job_name': 'test-spark-job'}
         self.mock_spark_job_service.delete_job.return_value = {
-            "message": "test-spark-job deleted from namespace test-namespace",
+            "message": "test-spark-job deleted",
             'status': 200
         }
         # Act
         response_body, response_code = yield self.send_request(body)
         # Assert
-        self.mock_spark_job_service.delete_job.assert_called_once_with('test-spark-job', 'test-namespace')
+        self.mock_spark_job_service.delete_job.assert_called_once_with('test-spark-job')
         self.mock_logger.debug.assert_has_calls([
-            call('Trying to delete job "test-spark-job" in namespace "test-namespace".'),
-            call('Deleting job "test-spark-job" in namespace "test-namespace" returned result "200".')
+            call('Trying to delete job "test-spark-job".'),
+            call('Deleting job "test-spark-job" returned result "200".')
         ])
         assert response_code == 200
         self.assertDictEqual(response_body, {
             'status': 'success',
             'data': {
-                'message': "test-spark-job deleted from namespace test-namespace"
+                'message': "test-spark-job deleted"
             }
         })
 
     @gen_test
     def test_delete_returns_message_and_status_code_when_k8s_error(self):
         # Arrange
-        body = {'job_name': 'test-spark-job', 'namespace': 'test-namespace'}
+        body = {'job_name': 'test-spark-job'}
         self.mock_spark_job_service.delete_job.return_value = {
             "message": "Kubernetes error",
             'status': 422
