3439248ee8e42f3a4584a0d34b4c5b4c89a92968
Robert Clegg <robert.clegg@tessella.com>
2019-03-25 15:23:58 +0000

    Begin is_valid_pod_name helper method

--- piezo_web_app/PiezoWebApp/src/utils/route_helper.py
+++ piezo_web_app/PiezoWebApp/src/utils/route_helper.py
@@ -1,3 +1,5 @@
+import re
+
 from PiezoWebApp.src.utils.str_helper import is_str_empty
 
 
@@ -5,3 +7,9 @@ def format_route_specification(name):
     if is_str_empty(name):
         raise ValueError("Route name must not be empty!")
     return r'/' + name + r'(|/)'
+
+
+def is_valid_pod_name(label):
+    match = re.match("^([0-9a-z])([0-9a-z]*)?$", label)
+    return match is not None
+
--- piezo_web_app/PiezoWebApp/tests/utils/route_helper_test.py
+++ piezo_web_app/PiezoWebApp/tests/utils/route_helper_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from PiezoWebApp.src.utils.route_helper import format_route_specification
+from PiezoWebApp.src.utils.route_helper import is_valid_pod_name
 
 
 def test_format_route_specification_returns_flexible_route_for_appropriate_name():
@@ -17,3 +18,13 @@ def test_format_route_specification_raises_value_error_if_name_empty(name):
     with pytest.raises(ValueError) as exception_info:
         format_route_specification(name)
     assert str(exception_info.value) == "Route name must not be empty!"
+
+
+@pytest.mark.parametrize("label", ["acb123"])
+def test_is_valid_pod_name_returns_true_for_valid_labels(label):
+    assert is_valid_pod_name(label) is True
+
+
+@pytest.mark.parametrize("label", [".label"])
+def test_is_valid_pod_name_returns_false_for_invalid_labels(label):
+    assert is_valid_pod_name(label) is False
