aae5fe86bfedb3857c5ecfc8c1a7b5beab505485
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-25 10:01:18 +0000

    Ensure clean image is built

--- Spark/build_spark_image.sh
+++ Spark/build_spark_image.sh
@@ -1,5 +1,5 @@
 HARBOR_URL=$1
 
-sudo docker build -t piezo-spark-image:latest .
+sudo docker build --no-cache -t piezo-spark-image:latest .
 sudo docker tag piezo-spark-image:latest ${HARBOR_URL}/piezo-resources/piezo-spark-image:latest
 sudo docker push ${HARBOR_URL}/piezo-resources/piezo-spark-image:latest
