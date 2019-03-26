3b9c45c3c6ba778e21407cdcfab6064160d73c48
Robert Clegg <robert.clegg@tessella.com>
2019-03-26 11:17:56 +0000

    Add no job found test to write logs integration tests

--- piezo_web_app/PiezoWebApp/tests/integration_tests/write_logs_test.py
+++ piezo_web_app/PiezoWebApp/tests/integration_tests/write_logs_test.py
@@ -1,3 +1,8 @@
+import json
+
+from kubernetes.client.rest import ApiException
+import pytest
+from tornado.httpclient import HTTPClientError
 from tornado.testing import gen_test
 
 from PiezoWebApp.src.handlers.write_logs_handler import WriteLogsHandler
@@ -16,7 +21,7 @@ class TestWriteLogsToFileIntegration(BaseIntegrationTest):
     @gen_test
     def test_logs_from_k8s_written_to_storage(self):
         # Arrange
-        body = {'job_name': 'test-job', 'namespace': 'default'}
+        body = {'job_name': 'test-job'}
         self.mock_k8s_adapter.read_namespaced_pod_log.return_value = 'Log\nFile\nContent'
         # Act
         response_body, response_code = yield self.send_request(body)
@@ -29,3 +34,15 @@ class TestWriteLogsToFileIntegration(BaseIntegrationTest):
             'status': 'success',
             'data': {'message': 'Logs written to "outputs/test-job/log.txt" in bucket "kubernetes"'}
         })
+
+    @gen_test
+    def test_trying_to_get_status_of_non_existent_job_returns_404_with_reason(self):
+        # Arrange
+        body = {'job_name': 'test-spark-job'}
+        self.mock_k8s_adapter.read_namespaced_pod_log.side_effect = ApiException(status=404, reason="Not Found")
+        # Act
+        with pytest.raises(HTTPClientError) as exception:
+            yield self.send_request(body)
+        assert exception.value.response.code == 404
+        msg = json.loads(exception.value.response.body, encoding='utf-8')['data']
+        assert msg == 'Kubernetes error when trying to get logs for spark job "test-spark-job": Not Found'
