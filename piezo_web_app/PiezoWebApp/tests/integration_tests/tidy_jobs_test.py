7452a2c7644a3acdae1de63ffe51651d897118d0
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-27 15:54:06 +0000

    Add integration test file

---
+++ piezo_web_app/PiezoWebApp/tests/integration_tests/tidy_jobs_test.py
@@ -0,0 +1,30 @@
+from tornado.testing import gen_test
+
+from PiezoWebApp.src.handlers.tidy_jobs import TidyJobsHandler
+from PiezoWebApp.tests.integration_tests.base_integration_test import BaseIntegrationTest
+
+
+# str | The custom resource's group name
+CRD_GROUP = 'sparkoperator.k8s.io'
+
+# str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
+CRD_PLURAL = 'sparkapplications'
+
+# str | The custom resource's version
+CRD_VERSION = 'v1beta1'
+
+NAMESPACE = 'default'
+
+
+class TestTidyJobsIntegration(BaseIntegrationTest):
+    @property
+    def handler(self):
+        return TidyJobsHandler
+
+    @property
+    def standard_request_method(self):
+        return 'POST'
+
+    @gen_test
+    def test_post_successfully_writes_logs_and_deletes_completed_and_failed_jobs(self):
+        pass
