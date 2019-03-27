76c5832441e46de128e4f7f874d9fa42192e116b
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-27 14:37:33 +0000

    Add handler code

--- piezo_web_app/PiezoWebApp/src/handlers/tidy_jobs.py
+++ piezo_web_app/PiezoWebApp/src/handlers/tidy_jobs.py
@@ -1,4 +1,20 @@
+from tornado_json import schema
+
 from PiezoWebApp.src.handlers.base_handler import BaseHandler
 
 class TidyJobsHandler(BaseHandler):
-    pass
+    @schema.validate(
+        output_schema={
+            "type": "object",
+            "properties": {
+                "Jobs processed": {"type": "string"},
+                "Jobs untouched": {"type": "string"}
+            },
+        }
+    )
+    def POST(self, *args, **kwargs):
+        result = self._spark_job_service.tidy_jobs()
+        self._logger.debug(f'Tidying spark application returned: "{result["status"]}".')
+        self.check_request_was_completed_successfully(result)
+        del result['status']
+        return result
