2d3efcac34e7e82af852ca4c659b3f122f06e118
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-03-27 16:54:04 +0000

    Assertions in integration test

--- piezo_web_app/PiezoWebApp/tests/integration_tests/tidy_jobs_test.py
+++ piezo_web_app/PiezoWebApp/tests/integration_tests/tidy_jobs_test.py
@@ -62,7 +62,7 @@ class TestTidyJobsIntegration(BaseIntegrationTest):
                                     "state": "COMPLETED"
                                 }
                         }
-                }
+                },
                 {
                     "metadata":
                         {
@@ -83,3 +83,39 @@ class TestTidyJobsIntegration(BaseIntegrationTest):
             ]
         }
         self.mock_k8s_adapter.list_namespaced_custom_object.return_value = kubernetes_list_jobs_response
+        self.mock_k8s_adapter.delete_options.return_value = {"api_version": "version", "other_values": "values"}
+        # Act
+        response_body, response_code = yield self.send_request_without_body()
+        # Assert
+        self.mock_k8s_adapter.read_namespaced_pod_log.assert_any_call('job2-driver', 'default')
+        self.mock_k8s_adapter.read_namespaced_pod_log.assert_any_call('job3-driver', 'default')
+        assert self.mock_k8s_adapter.read_namespaced_pod_log.call_count == 2
+        self.mock_k8s_adapter.delete_namespaced_custom_object.assert_any_call(CRD_GROUP,
+                                                                              CRD_VERSION,
+                                                                              NAMESPACE,
+                                                                              CRD_PLURAL,
+                                                                              'job2',
+                                                                              {
+                                                                                "api_version": "version",
+                                                                                "other_values": "values"
+                                                                              })
+        self.mock_k8s_adapter.delete_namespaced_custom_object.assert_any_call(CRD_GROUP,
+                                                                              CRD_VERSION,
+                                                                              NAMESPACE,
+                                                                              CRD_PLURAL,
+                                                                              'job3',
+                                                                              {
+                                                                                "api_version": "version",
+                                                                                "other_values": "values"
+                                                                              })
+        assert self.mock_k8s_adapter.delete_namespaced_custom_object.call_count == 2
+        assert response_code == 200
+        self.assertDictEqual(response_body, {
+            'status': 'success',
+            'data': {
+                'message': 'Spark jobs tidied successfully',
+                'Jobs tidied': 2,
+                'Jobs untouched': 1
+            }
+        })
+
