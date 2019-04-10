4b09f7870eb71caaa50ac4413d0f159d9a886930
Robert Clegg <robert.clegg@tessella.com>
2019-04-10 17:00:54 +0100

    Fix bucket name in boto adapter

--- piezo_web_app/PiezoWebApp/src/services/storage/adapters/boto_adapter.py
+++ piezo_web_app/PiezoWebApp/src/services/storage/adapters/boto_adapter.py
@@ -26,28 +26,25 @@ class BotoAdapter(IStorageAdapter):
             self._logger.critical(exception)
             raise exception
 
+        bucket_name = configuration.s3_self.s3_bucket_name
+        self._bucket = self._s3_client.lookup(bucket_name)
+        if self._bucket is None:
+            self._bucket = self._s3_client.create_bucket(bucket_name, location="")
+
         self._temp_url_expiry_seconds = configuration.temp_url_expiry_seconds
 
     def get_temp_url_for_each_file(self, bucket_name, file_prefix):
-        bucket = self._get_bucket(bucket_name)
-        keys = bucket.get_all_keys(prefix=file_prefix)
+        keys = self._bucket.get_all_keys(prefix=file_prefix)
         temp_urls = {key.name: key.generate_url(self._temp_url_expiry_seconds, method='GET') for key in keys}
         return temp_urls
 
     def set_contents_from_string(self, bucket_name, file_path, text):
-        key = self._get_key(bucket_name, file_path)
+        key = self._get_key(file_path)
         contents_size = key.set_contents_from_string(text)
         self._logger.debug(f'Wrote {contents_size} bytes to "{file_path}" in bucket "{bucket_name}"')
 
-    def _get_bucket(self, bucket_name):
-        bucket = self._s3_client.lookup(bucket_name)
-        if bucket is None:
-            raise RuntimeError(f'Bucket "{bucket_name}" does not exist')
-        return bucket
-
-    def _get_key(self, bucket_name, file_path):
-        bucket = self._get_bucket(bucket_name)
-        key = bucket.get_key(file_path)
+    def _get_key(self, file_path):
+        key = self._bucket.get_key(file_path)
         if key is None:
-            key = bucket.new_key(file_path)
+            key = self._bucket.new_key(file_path)
         return key
