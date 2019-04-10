0957235de9878050040578540c2ee0bd7811e92a
Robert Clegg <robert.clegg@tessella.com>
2019-04-10 08:39:57 +0100

    Update temp url tests with http protocol

--- piezo_web_app/PiezoWebApp/tests/integration_tests/output_files_integration_test.py
+++ piezo_web_app/PiezoWebApp/tests/integration_tests/output_files_integration_test.py
@@ -23,9 +23,9 @@ class OutputFilesIntegrationTest(BaseIntegrationTest):
         # Arrange
         body = {'job_name': 'test-job-abc12'}
         self.mock_storage_adapter.get_temp_url_for_each_file.return_value = {
-            'outputs/test-job-abc12/log.txt': 's3://log.txt.temp.url',
-            'outputs/test-job-abc12/output1.csv': 's3://output1.csv.temp.url',
-            'outputs/test-job-abc12/output2.csv': 's3://output2.csv.temp.url'
+            'outputs/test-job-abc12/log.txt': 'http://log.txt.temp.url',
+            'outputs/test-job-abc12/output1.csv': 'http://output1.csv.temp.url',
+            'outputs/test-job-abc12/output2.csv': 'http://output2.csv.temp.url'
         }
         # Act
         response_body, response_code = yield self.send_request(body)
@@ -38,9 +38,9 @@ class OutputFilesIntegrationTest(BaseIntegrationTest):
             'data': {
                 'message': 'Got temporary URLs for 3 output files for job "test-job-abc12"',
                 'files': {
-                    'log.txt': 's3://log.txt.temp.url',
-                    'output1.csv': 's3://output1.csv.temp.url',
-                    'output2.csv': 's3://output2.csv.temp.url'
+                    'log.txt': 'http://log.txt.temp.url',
+                    'output1.csv': 'http://output1.csv.temp.url',
+                    'output2.csv': 'http://output2.csv.temp.url'
                 }
             }
         })
