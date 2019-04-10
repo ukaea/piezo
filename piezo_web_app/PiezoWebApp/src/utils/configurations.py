26d93a2caeffc21a90cdda90e1b297189539b298
Robert Clegg <robert.clegg@tessella.com>
2019-04-10 16:52:29 +0100

    Add S3BucketName to configuration

--- piezo_web_app/PiezoWebApp/src/utils/configurations.py
+++ piezo_web_app/PiezoWebApp/src/utils/configurations.py
@@ -26,6 +26,7 @@ class Configuration:
 
         # Storage
         self._s3_endpoint = None
+        self._s3_bucket_name = None
         self._s3_secrets_name = None
         self._secrets_dir = None
         self._is_s3_secure = None
@@ -72,6 +73,10 @@ class Configuration:
         return parse_result.port
 
     @property
+    def s3_bucket_name(self):
+        return self._s3_bucket_name
+
+    @property
     def s3_secrets_name(self):
         return self._s3_secrets_name
 
@@ -107,6 +112,7 @@ class Configuration:
 
         # Storage
         self._s3_endpoint = storage['S3Endpoint']
+        self._s3_bucket_name = storage['S3BucketName']
         self._s3_secrets_name = storage['S3KeysSecret']
         self._secrets_dir = storage['SecretsDir']
         self._temp_url_expiry_seconds = str2non_negative_int(storage['TempUrlExpirySeconds'])
