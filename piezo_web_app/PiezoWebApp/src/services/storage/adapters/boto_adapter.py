83c2553c8bc5c7e505451d86367981d526f56d31
Robert Clegg <robert.clegg@tessella.com>
2019-04-11 08:36:24 +0100

    Refactor boto_adapter initialisation

--- piezo_web_app/PiezoWebApp/src/services/storage/adapters/boto_adapter.py
+++ piezo_web_app/PiezoWebApp/src/services/storage/adapters/boto_adapter.py
@@ -8,30 +8,10 @@ from PiezoWebApp.src.services.storage.adapters.i_storage_adapter import IStorage
 class BotoAdapter(IStorageAdapter):
     def __init__(self, configuration, logger):
         self._logger = logger
-
-        with open(os.path.join(configuration.secrets_dir, 'access_key')) as key_file:
-            access_key = key_file.read()
-        with open(os.path.join(configuration.secrets_dir, 'secret_key')) as key_file:
-            secret_key = key_file.read()
-        try:
-            self._s3_client = boto.connect_s3(
-                aws_access_key_id=access_key,
-                aws_secret_access_key=secret_key,
-                host=configuration.s3_host,
-                port=configuration.s3_port,
-                is_secure=configuration.is_s3_secure,
-                calling_format=boto.s3.connection.OrdinaryCallingFormat()
-            )
-        except Exception as exception:
-            self._logger.critical(exception)
-            raise exception
-
-        bucket_name = configuration.s3_self.s3_bucket_name
-        self._bucket = self._s3_client.lookup(bucket_name)
-        if self._bucket is None:
-            self._bucket = self._s3_client.create_bucket(bucket_name, location="")
-
         self._temp_url_expiry_seconds = configuration.temp_url_expiry_seconds
+
+        self._s3_client = self._get_client(configuration)
+        self._bucket = self._get_bucket(configuration.s3_bucket_name, self._s3_client)
 
     def get_temp_url_for_each_file(self, bucket_name, file_prefix):
         keys = self._bucket.get_all_keys(prefix=file_prefix)
@@ -42,6 +22,34 @@ class BotoAdapter(IStorageAdapter):
         key = self._get_key(file_path)
         contents_size = key.set_contents_from_string(text)
         self._logger.debug(f'Wrote {contents_size} bytes to "{file_path}" in bucket "{bucket_name}"')
+
+    def _get_bucket(self, bucket_name, client):
+        try:
+            bucket = client.lookup(bucket_name)
+            if bucket is None:
+                bucket = client.create_bucket(bucket_name, location="")
+            return bucket
+        except Exception as exception:
+            self._logger.critical(exception)
+            raise exception
+
+    def _get_client(self, configuration):
+        with open(os.path.join(configuration.secrets_dir, 'access_key')) as key_file:
+            access_key = key_file.read()
+        with open(os.path.join(configuration.secrets_dir, 'secret_key')) as key_file:
+            secret_key = key_file.read()
+        try:
+            return boto.connect_s3(
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                host=configuration.s3_host,
+                port=configuration.s3_port,
+                is_secure=configuration.is_s3_secure,
+                calling_format=boto.s3.connection.OrdinaryCallingFormat()
+            )
+        except Exception as exception:
+            self._logger.critical(exception)
+            raise exception
 
     def _get_key(self, file_path):
         key = self._bucket.get_key(file_path)
