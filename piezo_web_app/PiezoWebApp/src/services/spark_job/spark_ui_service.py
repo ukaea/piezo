41e31623b21cbad1cb101b7ef83fb67b1980e5be
Oliver Tarrant <oliver.tarrant@tessella.com>
2019-04-17 12:07:52 +0100

    Neaten host extraction

--- piezo_web_app/PiezoWebApp/src/services/spark_job/spark_ui_service.py
+++ piezo_web_app/PiezoWebApp/src/services/spark_job/spark_ui_service.py
@@ -57,8 +57,8 @@ class SparkUiService(ISparkUiService):
         backend = kubernetes.client.V1beta1IngressBackend(service_name=service_name, service_port=80)
         ingress_path = kubernetes.client.V1beta1HTTPIngressPath(backend=backend, path=path)
         http = kubernetes.client.V1beta1HTTPIngressRuleValue(paths=[ingress_path])
-        host = self._k8s_url.split("//")[1]
-        host = host.split(':')[0]
+        host_with_port = self._k8s_url.split("//")[-1]
+        host = host_with_port.split(':')[0]
         rules = kubernetes.client.V1beta1IngressRule(host=host, http=http)
         spec = kubernetes.client.V1beta1IngressSpec(backend=backend, rules=[rules])
         return kubernetes.client.V1beta1Ingress(api_version='extensions/v1beta1',
