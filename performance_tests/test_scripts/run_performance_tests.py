1a6f43cd92802f96f6345a8aa3df5346641f4753
Robert Clegg <robert.clegg@tessella.com>
2019-04-29 11:03:58 +0100

    Uncomment test options

--- performance_tests/test_scripts/run_performance_tests.py
+++ performance_tests/test_scripts/run_performance_tests.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     piezo_base_url = 'http://host-172-16-113-146.nubes.stfc.ac.uk:31856/piezo'
     job_options = [
         SubmitJobOptions(driver_cores=0.1, driver_memory="512m", executors=1, executor_cores=1, executor_memory="512m"),
-        #SubmitJobOptions(driver_cores=1.0, driver_memory="2048m", executors=10, executor_cores=4, executor_memory="4096m")
+        SubmitJobOptions(driver_cores=1.0, driver_memory="2048m", executors=10, executor_cores=4, executor_memory="4096m")
     ]
     system_load_results = system_load_tests.run_system_load_tests(piezo_base_url, job_numbers, job_options)
 
